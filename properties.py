cfd193d513a7a72408bdd917ae6811cda9e8ec4a
Bowen Wu <fsdi321@mit.edu>
2022-06-14 00:04:33 -0400

    Now keeps properties regardless of redo when timelapse is running

--- properties.py
+++ properties.py
@@ -24,43 +24,29 @@ def update_counter_on_dir_change(self, context):
 
 # Code adapted from https://blenderartists.org/t/prevent-panel-from-receiving-undo-hotkey-event/1192545/9
 # Needed so that undo/redo does not affect these two attributes for the sake of consistent operation
-
-
 def restore_tl_props(scene):
     def restore_tl_prop(prop_name):
         stored_prop = getattr(restore_tl_props, prop_name, None)
         current_prop = getattr(scene.tl, prop_name, None)
-        print("checking " + prop_name)
-        print("stored prop", stored_prop)
-        print("current prop", current_prop)
         if stored_prop is not None and current_prop != stored_prop:
-            print("restoring", prop_name, "to ",
-                  stored_prop, "was ", current_prop)
             setattr(scene.tl, prop_name, stored_prop)
-
-    restore_tl_prop("num_screenshots")
     restore_tl_prop("is_running")
-
-    # if is_running:
-    #     seconds_per_frame = getattr(restore_tl_props, "seconds_per_frame", None)
-    #     output_name = getattr(restore_tl_props, "output_name", None)
-    #     file_format = getattr(restore_tl_props, "file_format", None)
-    #     is_running = getattr(restore_tl_props, "is_running", None)
+    restore_tl_prop("num_screenshots")
+    is_running = getattr(restore_tl_props, "is_running", None)
+    # Prevent these settings from being changed while a timelapse is running
+    if is_running:
+        restore_tl_prop("seconds_per_frame")
+        restore_tl_prop("output_name")
+        restore_tl_prop("file_format")
+        restore_tl_prop("enable_screenshot_notification")
+        restore_tl_prop("remind_resume")
+        restore_tl_prop("auto_resume")
+        restore_tl_prop("dir_path")
 
 
 def store_tl_props(self, context, prop_name):
     prop_to_store = getattr(self, prop_name, None)
     setattr(restore_tl_props, prop_name, prop_to_store)
-    # restore_tl_props.num_screenshots = self.num_screenshots
-    # restore_tl_props.is_running = self.is_running
-    # if self.is_running: #Prevent settings from changing due to a undo/redo while a timelapse is taking place
-    #     restore_tl_props.seconds_per_frame = self.seconds_per_frame
-    #     restore_tl_props.output_name = self.output_name
-    #     restore_tl_props.file_format = self.file_format
-    #     restore_tl_props.enable_screenshot_notification = self.enable_screenshot_notification
-    #     restore_tl_props.remind_resume = self.remind_resume
-    #     restore_tl_props.auto_resume = self.auto_resume
-    #     restore_tl_props.dir_path = self.dir_path
 
 
 class Timelapse_Addon_Properties(bpy.types.PropertyGroup):
@@ -70,13 +56,17 @@ class Timelapse_Addon_Properties(bpy.types.PropertyGroup):
         min=1,
         default=10,
         subtype="TIME",
-        unit="TIME"
+        unit="TIME",
+        update=lambda self, context: store_tl_props(
+            self, context, "seconds_per_frame")
     )
 
     output_name: bpy.props.StringProperty(
         name="Output Name",
         description="Name of the output files, no extensions",
-        default='timelapse-screenshot'
+        default='timelapse-screenshot',
+        update=lambda self, context: store_tl_props(
+            self, context, "output_name")
     )
 
     file_format: bpy.props.EnumProperty(
@@ -88,12 +78,16 @@ class Timelapse_Addon_Properties(bpy.types.PropertyGroup):
             ('tiff', 'TIFF', ''),
             ('exr', 'OpenEXR', ''),
         ),
-        default='png'
+        default='png',
+        update=lambda self, context: store_tl_props(
+            self, context, "file_format")
     )
 
     enable_screenshot_notification: bpy.props.BoolProperty(
         name="",
-        default=False
+        default=False,
+        update=lambda self, context: store_tl_props(
+            self, context, "enable_screenshot_notification")
     )
 
     num_screenshots: bpy.props.IntProperty(
@@ -106,20 +100,28 @@ class Timelapse_Addon_Properties(bpy.types.PropertyGroup):
 
     remind_resume: bpy.props.BoolProperty(
         name="",
-        default=True
+        default=True,
+        update=lambda self, context: store_tl_props(
+            self, context, "remind_resume")
     )
 
     auto_resume: bpy.props.BoolProperty(
         name="",
         default=False,
+        update=lambda self, context: store_tl_props(
+            self, context, "auto_resume")
     )
+
+    def dir_path_update_func(self, context):
+        store_tl_props(self, context, "dir_path")
+        update_counter_on_dir_change(self, context)
 
     dir_path: bpy.props.StringProperty(
         name="",
         description="timelapse screenshot output directory path",
         default="//timelapse_screenshots/",
         subtype='DIR_PATH',
-        update=update_counter_on_dir_change
+        update=dir_path_update_func
     )
 
     is_running: bpy.props.BoolProperty(
